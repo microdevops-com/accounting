3be27abe4ce50e9d717c28e4a7a5877396e6bdbe
Eugene Antonov <jetman@sysadm.ws>
2022-01-05 16:48:52 +0200

    pre push hook yaml check

--- .git_pull.sh
+++ .git_pull.sh
@@ -5,3 +5,4 @@ git fetch --prune origin +refs/tags/*:refs/tags/*
 git submodule init
 git submodule update -f --checkout
 git submodule foreach "git checkout master && git pull && git fetch --prune origin +refs/tags/*:refs/tags/*"
+ln -sf ../../.githooks/pre-push .git/hooks/pre-push
